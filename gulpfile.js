e5a9bde092b5885607ab40815bd658bb7c914e1a
Manuel Schulz <manuel@cologne-systems.de>
2015-03-25 10:53:53 +0100

    gulp plugins installiert; default tastk + sass;

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    prefix = require('gulp-autoprefixer'),
+    minify = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
+
+
 
 gulp.task('default', function() {
-  // place code for your default task here
+    return gulp.src('src/scss/main.scss')
+            .pipe(sass())
+            .pipe(prefix({
+                browsers: ['last 2 versions','ie 9']
+            }))
+            .pipe(rename('style.css'))
+            .pipe(gulp.dest('dist/src/css'));
 });
