0c9968d53402364148d096e1b41ad4b4f8e52ac3
Manuel Schulz <manuel@cologne-systems.de>
2015-04-29 11:01:37 +0200

    initial cv timeline commit

--- dist/index.php
+++ dist/index.php
@@ -9,6 +9,18 @@ $twig = new Twig_Environment($loader, array(
     #'debug' => true,
 ));
 
-echo $twig->render('index.html',array("value"=>"basekit"));
+$page = (isset($_GET['page'])) ? $_GET['page'] : false;
+
+
+if(!$page || $page == '') {
+	echo $twig->render('index.html',array("value"=>"basekit"));
+}
+elseif(!is_file('./templates/'.$page.'.html')) {
+    header("HTTP/1.0 404 Not Found");
+    echo $twig->render('404.html');
+}
+else {
+    echo $twig->render($page.'.html',array());
+}
 
 ?>
